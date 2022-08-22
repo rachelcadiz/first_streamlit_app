e6d50c0e07360458bdaf06c05e99862ffc0bb433
rachelcadiz <111741419+rachelcadiz@users.noreply.github.com>
2022-08-22 17:55:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
 Streamlit.title('Omega 3 & Blueberry Oatmeal')
-Streamlit.title('Kale, Spinach \& Rocket Smoothie')
+Streamlit.title('Kale, Spinach &&& Rocket Smoothie')
 Streamlit.title('Hard-Boiled Free-Range Egg')
