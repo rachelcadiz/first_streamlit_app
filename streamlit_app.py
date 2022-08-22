4bd95ba8ac825d23c9d4a14d3eed6ab1e8d3a741
rachelcadiz <111741419+rachelcadiz@users.noreply.github.com>
2022-08-22 17:51:54 +0800

    Update streamlit_app.py
    add more titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Breakfast Menu')
+Streamlit.title('Omega 3 & Blueberry Oatmeal')
+Streamlit.title('Kale, Spinach & Rocket Smoothie')
+Streamlit.title('Hard-Boiled Free-Range Egg')
