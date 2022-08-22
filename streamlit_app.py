5536ce54248b37e8eb8a1d11b4b5c4595b7318f9
rachelcadiz <111741419+rachelcadiz@users.noreply.github.com>
2022-08-22 18:02:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
-print("Omega 3 Blueberry Oatmeal")
+streamlit.print("Omega 3 Blueberry Oatmeal")
 print("Kale, Spinach && Rocket Smoothie")
 print('Hard-Boiled Free-Range Egg')
